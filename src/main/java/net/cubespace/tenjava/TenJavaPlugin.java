65acce1ffff063ce29d9b670ce1f9d100c851ca4
Fabian Fassbender <fabian.fassbender42@googlemail.com>
2013-12-07 10:34:29 +0100

    Initial Skeleton :D

---
+++ src/main/java/net/cubespace/tenjava/TenJavaPlugin.java
@@ -0,0 +1,19 @@
+package net.cubespace.tenjava;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author geNAZt (fabian.fassbender42@googlemail.com)
+ * @date Last changed: 07.12.13 10:09
+ */
+public class TenJavaPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
